06b2f76f62ee49dadf430e2f487499a8ae60e6bc
Marcelo Diefenbach <58398373+MarceloDiefenbach@users.noreply.github.com>
2023-04-23 11:20:55 -0300

    Initial Commit

---
+++ Events/EventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventsApp.swift
+//  Events
+//
+//  Created by Marcelo Diefenbach on 23/04/23.
+//
+
+import SwiftUI
+
+@main
+struct EventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
